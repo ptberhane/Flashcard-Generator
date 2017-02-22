7257d237373b562e9814a0b8f634aed4baec1eaa
ptberhane <piniel.berhane@gmail.com>
2017-02-21 23:50:19 -0500

    updated files

--- cards.js
+++ cards.js
@@ -5,15 +5,23 @@ function Basic (front, back){
 	this.front = front;
 	this.back = back;
 };
+function usersafeBasic(front,back){
+	return new Basic (front, back)
+};
 
-function Cloze (cloze,partial){
-	this.cloze =cloze;
+function Cloze (full,partial){
+	this.full=full;
 	this.partial = partial;
-}
-var firstPresident = new Basic(
+};
+function usersafeCloze(front,back){
+	return new Cloze(full,partial)
+};
+var firstPresident = usersafeBasic(
     "Who was the first president of the United States?", "George Washington");
-
+var firstPresidentpartial = usersafeCloze("....was the first president of the United States");
+var	firstPresidentfull = usersafeCloze("George Washington was the first president of the United States.");
 /*console.log(firstPresident.front);*/
+promptUser();
 
 function promptUser(){
 	inquirer.prompt([{
@@ -25,8 +33,20 @@ function promptUser(){
 	}]).then (function(user){
 		if (user.types==="basic"){
 			console.log(firstPresident.front);
-		then (
-			console.log(firstPresident.back));
-		}
-	})
-};
+			console.log(firstPresident.back);	
+			};
+			if (user.types ==="cloze"){
+				type:"rawlist"
+				message : "Partial or full flashcard?"
+				choices :["partial","full"],
+				name: "options",
+			}.then function(user){
+
+			else if  (user.options==="partial"){
+				console.log(firstPresidentpartial);
+			}
+			else {
+				console.log(firstPresidentfull);
+			};
+	});
+}
